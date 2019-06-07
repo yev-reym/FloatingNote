5d342af3973a7100ea6a791b729a34cb103a9ae1
App Academy Student <yevgeniyreym@gmail.com>
2019-06-07 13:24:25 -0400

    Change track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,8 @@
 class Track < ApplicationRecord
     validates :title, :uploader_id, presence: true
 
-    has_one_attached :track_file, :photo
+    has_one_attached :track_file
+    has_one_attached :photo
 
     belongs_to :uploader,
         foreign_key: :uploader_id,
